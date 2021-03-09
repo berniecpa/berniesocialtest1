fe36a7822371c5a9f3c6ba871d647c7941e27304
Laravel Daily <info@laraveldaily.com>
2021-03-09 22:10:08 +0000

    QuickAdminPanel automatic commit

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -15,12 +15,7 @@
                         </div>
                     @endif
 
-                    <div class="row">
-                        <div class="{{ $chart1->options['column_class'] }}">
-                            <h3>{!! $chart1->options['chart_title'] !!}</h3>
-                            {!! $chart1->renderHtml() !!}
-                        </div>
-                    </div>
+                    You are logged in!
                 </div>
             </div>
         </div>
@@ -29,6 +24,5 @@
 @endsection
 @section('scripts')
 @parent
-<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.5.0/Chart.min.js"></script>
-{!! $chart1->renderJs() !!}
+
 @endsection
